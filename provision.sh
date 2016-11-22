2fe8ac1aef068aa59c8791ec35f5a9abbc71bb1c
Girish Pandit <girschol7@gmail.com>
2016-11-21 21:41:47 -0800

    add missing steps in provision.sh

--- provision.sh
+++ provision.sh
@@ -1,4 +1,8 @@
-sudo add-apt-repository -y ppa:webupd8team/java
+#/usr/bin/env bash
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java9-installer
 sudo apt-get install -qqy oracle-java9-installer
 sudo apt-get install -qqy oracle-java9-set-default
 export JAVA_HOME=/usr/lib/jvm/java-9-oracle
